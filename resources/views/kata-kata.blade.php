9689ff9b60157aa7c48350c81d3559940ea80650
PutraOutlash <putraoulash@gmail.com>
2026-02-11 09:37:12 +0700

    Menambahkan kata' dan memanggil di router

---
+++ resources/views/kata-kata.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tugas Github </title>
+</head>
+
+<body>
+    <h1>Aku sudah ngepuss dan ngepull</h1>
+    <p>bangga menjadi mahasiswa polije</p>
+    <hr>
+    <p>Status: <strong>Berhasil Terintegrasi</strong></p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route bawaan Laravel
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route baru untuk tes kamu (taruh di luar, jangan di dalam yang lain)
+Route::get('/tes-push', function () {
+    return "<h1>Pria Solo !</h1><p>aku adalah pria  solo yang sangat amat sigma cuyy !!.</p>";
+});
+Route::get('/laporan-aku', function () {
+    return view('kata-kata');
 });
